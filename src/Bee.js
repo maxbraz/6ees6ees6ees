af3d97e83427d6776b328033b06bb79da216a167
Max Braz <braz.max@gmail.com>
2017-04-03 11:57:47 -0700

    add Grub & Bee class

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,29 @@
-class Bee {
-  // TODO..
-};
+class Bee extends Grub {
+  constructor () {
+    super();
+    this.age = 5;
+    this.color = 'yellow';
+    this.job = 'Keep on growing';
+  }
+}
+
+
+// class Car {
+//   constructor () {
+//     this.fuel = 0
+//     this.distance = 0
+//   }
+//   move () {
+//     if (this.fuel < 1) {
+//       throw new RangeError('Fuel tank is depleted')
+//     }
+//     this.fuel--
+//     this.distance += 2
+//   }
+//   addFuel () {
+//     if (this.fuel >= 60) {
+//       throw new RangeError('Fuel tank is full')
+//     }
+//     this.fuel++
+//   }
+// }
